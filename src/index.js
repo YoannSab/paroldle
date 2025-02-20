09f9a952d083667c3edd93a9abfad43274222f70
Yoann Sabatier <82182246+YoannSab@users.noreply.github.com>
2025-02-20 15:18:34 -0500

    [UPD] Hashrouter dom

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './components/App';
 import AppChill from './componentsChill/App';
 import './compare_words';
@@ -10,8 +10,8 @@ ReactDOM.render(
   <ChakraProvider>
     <Router>
       <Routes>
-        <Route path="/paroldle" element={<App />} />
-        <Route path="/paroldle/chill" element={<AppChill />} />
+        <Route path="/" element={<App />} />
+        <Route path="/chill" element={<AppChill />} />
       </Routes>
     </Router>
   </ChakraProvider>,
